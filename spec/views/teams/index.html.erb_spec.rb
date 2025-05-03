452641ef035d08b5de301784178b5a092dd1b8b4
Peter Bell <peter.bell5@ntlworld.com>
2025-05-03 05:41:02 +0100

    Adds team management functionality
    This commit introduces the ability to manage teams, including creating, reading, updating, and deleting them.
    
    It also adds a link to the teams index page in the application layout.

---
+++ spec/views/teams/index.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "teams/index", type: :view do
+  before(:each) do
+    assign(:teams, [
+      Team.create!(),
+      Team.create!()
+    ])
+  end
+
+  it "renders a list of teams" do
+    render
+    cell_selector = 'div>p'
+  end
+end
