b0aa07ae62e8858fe98dc98ca95bf128cca5c6f0
Peter Bell <peter.bell5@ntlworld.com>
2025-05-03 04:26:10 +0100

    Adds default admin user on seed
    Creates a default admin user with predefined credentials
    during database seeding. This ensures an admin user is
    available upon initial application setup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create default admin user
+admin = User.find_or_create_by!(email: "peter.bell@modaxo.com") do |user|
+  user.name = "Peter Bell"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.user_type = "admin"
+end
+
+puts "Default admin user created: #{admin.email}"
