a5320efcaaffdcf7a82bac01af3bde0221233c5f
Peter Bell <peter.bell5@ntlworld.com>
2025-05-04 20:06:22 +0100

    Updates user factory for improved testing
    Refactors the user factory to use sequences for names and emails, ensuring uniqueness across test runs. Also, updates the email domains to reflect current standards.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,32 +1,27 @@
 FactoryBot.define do
   factory :user do
-    skip_invitation { true }
-
+    skip_invitation     { true }
+    password            { "password" }
+    
     factory :team_member do
-      name                  { "Team Member" }
-      email                 { "team.member@example.com" }
-      password              { "password" }
-      password_confirmation { "password" }
-      user_type             { :team_member }
+      sequence(:name)   { |n| "Team Member #{n}" }
+      sequence(:email)  { |n| "team.member#{n}@modaxo.com" }
+      user_type         { :team_member }
       team
     end
 
     factory :judge do
-      name                  { "Judge" }
-      email                 { "judge@example.com" }
-      password              { "password" }
-      password_confirmation { "password" }
-      user_type             { :judge }
-      team                  { nil }
+      sequence(:name)   { |n| "Judge #{n}" }
+      sequence(:email)  { |n| "judge#{n}@modaxo.com" }
+      user_type         { :judge }
+      team              { nil }
     end
 
     factory :admin do
-      name                  { "Admin" }
-      email                 { "admin@example.com" }
-      password              { "password" }
-      password_confirmation { "password" }
-      user_type             { :admin }
-      team                  { nil }
+      sequence(:name)   { |n| "Admin #{n}" }
+      sequence(:email)  { |n| "admin#{n}@modaxo.com" }
+      user_type         { :admin }
+      team              { nil }
     end
   end
 end
